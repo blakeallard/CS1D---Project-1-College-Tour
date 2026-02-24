1e4bec2097eab810e251d1e4aeafd46cd04c33bf
Khaled Ajaj <khaledajaj2026@gmail.com>
2026-02-23 21:19:37 -0800

    Set up React Router to make multiple pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,13 @@
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
 import App from './App.jsx'
+import About from './About.jsx'
 
-createRoot(document.getElementById('root')).render(<App />)
+createRoot(document.getElementById('root')).render(
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<App />} />
+        </Routes>
+    </BrowserRouter>
+)
