feffa1ea587c3c41ed74cb43fb825c5261eaee80
Khaled Ajaj <khaledajaj2026@gmail.com>
2026-02-24 23:49:40 -0800

    Display all campus information in Campuses page
    Campus page displays all campuses, their names and distances, in table format

--- frontend/src/pages/Campuses/Campuses.jsx
+++ frontend/src/pages/Campuses/Campuses.jsx
@@ -1,10 +1,46 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
 import HomeButton from "../../components/HomeButton/HomeButton";
+import "./Campuses.css";
 
 export default function Campuses() {
+    const [campuses, setCampuses] = new useState([]);
+
+    const handleCampuses = (campusData) => {
+        setCampuses(campusData);
+    }
+
+    useEffect(() => {
+        axios.get("/api/University/all")
+            .then(response => {
+                const allCampuses = response.data.campuses;
+                // console.log(allCampuses);
+                handleCampuses(allCampuses);
+            });
+    }, []);
+
     return (
         <>
             <HomeButton />
-            <h1>Campuses</h1>
+            <h1>Campus Information</h1>
+            <div className="campus_info-container">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Distance from Saddleback</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {campuses.map(campus => (
+                            <tr>
+                                <td>{campus.name}</td>
+                                <td>{campus.distance}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://0.0.0.0:18080',
+        changeOrigin: true
+      }
+    }
+  },
   plugins: [react()],
 })
