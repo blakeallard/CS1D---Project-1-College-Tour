9eb64dae138ddb9db08445cad3ee7157cabc5c38
Khaled Ajaj <khaledajaj2026@gmail.com>
2026-02-24 20:19:03 -0800

    Create navigation for Tours page

--- frontend/src/pages/Tours/Tours.jsx
+++ frontend/src/pages/Tours/Tours.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router";
 import HomeButton from "../../components/HomeButton/HomeButton";
 import "./Tours.css";
 
@@ -9,9 +10,12 @@ export default function Tours() {
                 <h1 className="tours-header">Select a Tour</h1>
             </header>
             <main>
-                <div className="menu_selection">
-                    
-                </div>
+                <nav className="tours-nav_buttons-container">
+                    <NavLink className="nav-button" to={"/saddleback-start"}>Saddleback</NavLink>
+                    <NavLink className="nav-button" to={"/asu-start"}>ASU</NavLink>
+                    <NavLink className="nav-button" to={"/uci-start"}>UCI</NavLink>
+                    <NavLink className="nav-button" to={"/custom-tour"}>Create a Custom Tour</NavLink>
+                </nav>
             </main>
         </>
     );
