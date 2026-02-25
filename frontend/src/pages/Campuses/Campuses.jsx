c3e75bafb397d79a1bbbafe3b0cbb292e2b28420
Khaled Ajaj <khaledajaj2026@gmail.com>
2026-02-25 01:00:17 -0800

    Build souvenir display in Campuses page

--- frontend/src/pages/Campuses/Campuses.jsx
+++ frontend/src/pages/Campuses/Campuses.jsx
@@ -1,20 +1,74 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
 import HomeButton from "../../components/HomeButton/HomeButton";
+import cross from "../../assets/cross-svgrepo-com.svg";
 import "./Campuses.css";
+
+const SouvenirInfo = ({ handleSvnrClicked, campusName }) => {
+    const [souvenirs, setSouvenirs] = new useState([]);
+    const campusNameWithoutSpaces = campusName.replaceAll(' ', '%20');
+
+    const handleSouvenirs = (newSvnrs) => {
+        setSouvenirs(newSvnrs);
+    }
+
+    useEffect(() => {
+        axios.get(`/api/University/${campusNameWithoutSpaces}`)
+            .then(response => {
+                const svnrs = response.data.souvenirs;
+                handleSouvenirs(svnrs);
+            });
+    }, []);
+
+    return (
+        <div className="svnr_info-window">
+            <button className="close_window-button" onClick={handleSvnrClicked}>
+                <img className="close_window-cross" src={cross} alt="close window" />
+            </button>
+            <h2>Souvenirs for {campusName}</h2>
+            <table className="svnr-table">
+                <thead>
+                    <tr>
+                        <th>Item</th>
+                        <th>Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {souvenirs.map((svnr, i) => {
+                        return (
+                            <tr key={i}>
+                                <td>{svnr.item}</td>
+                                <td>{svnr.price}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
+}
 
 export default function Campuses() {
     const [campuses, setCampuses] = new useState([]);
-
+    const [campusName, setCampusName] = new useState("svnr");
+    const [svnrClicked, setSvnrClicked] = new useState(false);
+    
     const handleCampuses = (campusData) => {
         setCampuses(campusData);
+    }
+
+    const handleCampusName = (newCampusName) => {
+        setCampusName(newCampusName);
+    }
+    
+    const handleSvnrClicked = () => {
+        setSvnrClicked(!svnrClicked);
     }
 
     useEffect(() => {
         axios.get("/api/University/all")
             .then(response => {
                 const allCampuses = response.data.campuses;
-                // console.log(allCampuses);
                 handleCampuses(allCampuses);
             });
     }, []);
@@ -24,6 +78,9 @@ export default function Campuses() {
             <HomeButton />
             <h1>Campus Information</h1>
             <div className="campus_info-container">
+                {svnrClicked ?
+                    <SouvenirInfo handleSvnrClicked={handleSvnrClicked} campusName={campusName} />
+                : ''}
                 <table>
                     <thead>
                         <tr>
@@ -32,12 +89,21 @@ export default function Campuses() {
                         </tr>
                     </thead>
                     <tbody>
-                        {campuses.map(campus => (
-                            <tr>
-                                <td>{campus.name}</td>
-                                <td>{campus.distance}</td>
-                            </tr>
-                        ))}
+                        {campuses.map((campus, i) => {
+                            return (
+                                <tr key={i}>
+                                    <td>
+                                        <button className={"souvenir_link"} onClick={() => {
+                                            handleSvnrClicked();
+                                            handleCampusName(campus.name);
+                                        }}>
+                                            {campus.name}
+                                        </button>
+                                    </td>
+                                    <td>{campus.distance}</td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </table>
             </div>
